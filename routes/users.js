77e9f9c2509dc481ceca6780cd22e36e146e4bc0
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-08-01 21:00:51 -0300

    modified: cambia toda la extructura html a ejs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const usersController = require('../controllers/usersController');
+
+
+/* /user */
+router.get('/register', usersController.register);
+router.get('/login', usersController.login);
+router.get('/profile', usersController.profile);
 
 module.exports = router;
