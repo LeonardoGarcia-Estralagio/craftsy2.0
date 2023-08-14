239565bfb58acb8bb22d7e59490cfb3d6d1fbb3c
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-08-14 18:10:51 -0300

    feat: borrar productos

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -1,3 +1,10 @@
-module.exports = (res,res) => {
-    return res.send('remove')
+const {readJSON} = require('../../data')
+
+module.exports = (req,res) => {
+    const products = readJSON('products.json')
+    const id = req.params.id;
+
+    const productsModify = products.filter(product => product.id === id)
+    
+    return res.send('remover el producto')
 }
