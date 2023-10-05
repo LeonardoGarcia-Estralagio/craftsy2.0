8d4e5d035e8149623d0008e91090b537b5e73084
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-10-05 16:14:53 -0300

    create: modelo y migraciones de tabla origins

--- src/database/migrations/20230921232310-create-brand.js
+++ src/database/migrations/20230921232310-create-brand.js
@@ -1,32 +1,40 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Brands', {
+    await queryInterface.createTable("Brands", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      originId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Origins",
+          },
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Brands');
-  }
+    await queryInterface.dropTable("Brands");
+  },
 };
